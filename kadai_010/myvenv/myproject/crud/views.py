3cb454e708c89bab2235a06f14b8589610bc7a7a
shimane5050 <shimane5050@gmail.com>
2024-05-13 21:29:28 +0900

    kadai_010を追加

---
+++ kadai_010/myvenv/myproject/crud/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+class ProductListView(ListView):
+     model = Product
+
+class ProductDetailView(DetailView):
+     model = Product
